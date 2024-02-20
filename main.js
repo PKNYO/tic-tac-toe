0273a3253617b7c9cc551c524a19474d50f8f628
PKNYO <147757429+PKNYO@users.noreply.github.com>
2024-02-20 13:37:49 +0100

    Add check winner and check valid move function
    We can know play and get checked for valid pos
    and for a winner

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const GameBoard = (function() {
     const board = 
         [
+            ["X", "X", "O"],
             ["", "", ""],
-            ["", "", ""],
-            ["", "", ""]
+            ["O", "X", ""]
         ];
 
     const displayBoard = function() {
@@ -11,7 +11,38 @@ const GameBoard = (function() {
     };
 
     const positionMarker = function(marker, xPosition, yPosition) {
-        board[yPosition][xPosition] = marker;
+        if (validMove(xPosition, yPosition)) {
+            board[yPosition][xPosition] = marker;
+            if (isWinner()) return console.log("last player is the winner");
+        } else {
+            console.log("Please, position your marker on an empty case.")
+        }
+    }
+
+    const validMove = function(xPosition, yPosition) {
+        return (board[yPosition][xPosition] === "") ? true : false;
+    }
+
+    const isWinner = function() {
+        let columnResult = "";
+        let firstDiagonalResult = "";
+        let secondDiagonalResult = "";
+
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                if (board[y].join("") === "XXX" || board[y].flat() === "OOO") return true;
+                columnResult += board[y][x];
+                if (x == y) firstDiagonalResult += board[y][x];
+                if (x == 0 && y == 2) secondDiagonalResult += board[y][x];
+                if (x == 1 && y == 1) secondDiagonalResult += board[y][x];
+                if (x == 2 && y == 0) secondDiagonalResult += board[y][x];
+            }
+            if (columnResult === "XXX" || columnResult === "OOO") return true;
+            if (firstDiagonalResult === "XXX" || firstDiagonalResult === "OOO") return true;
+            if (secondDiagonalResult === "XXX" || secondDiagonalResult === "OOO") return true;
+        }
+
+        return false;
     }
 
     return {board, displayBoard, positionMarker};
